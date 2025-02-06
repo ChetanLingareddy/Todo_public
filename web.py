262a6720131bf3947a5b3c955e95bd6433fa1b03
ChetanLingareddy <lingareddychetan@gmail.com>
2025-02-05 18:06:59 -0600

    Made some changes

--- web.py
+++ web.py
@@ -5,22 +5,23 @@ todos = functions.get_todos()
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
-
     todos.append(todo)
-
     functions.write_todos(todos)
 
-st.title("My Todo List ☑️")
 
-for index, todo in enumerate(todos):
+st.title(" My Todo App ✅")
+
+for index , todo in enumerate(todos):
     checkbox = st.checkbox(todo, key = todo)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
         st.rerun()
-st.text_input(label= "", placeholder="Enter a new Todo.....",
-              on_change=add_todo, key="new_todo")
+
+st.text_input(label= "" , placeholder= "Enter a todo...." ,
+                       on_change= add_todo, key = "new_todo")
+
 
 
 
